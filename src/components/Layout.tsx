12ea76d55de968967a7cc952d7f2d5994f31d237
Thomas Roest <thomas.roest@moxio.com>
2024-11-12 15:41:12 +0100

    header

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,19 +5,23 @@ export const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="flex flex-col h-screen">
       <header className="bg-white p-6">
-        <ul className="flex gap-2">
+        <ul className="flex gap-2 items-center justify-between">
           <li>
-            <Link href="/">home</Link>
+            <Link href="/" className="text-4xl tracking-wider text-indigo-600">
+              Ploxie
+            </Link>
           </li>
-          <li>
-            <Link href="/ruimtes">ruimtes</Link>
-          </li>
-          <li>
-            <Link href="/plattegrond">plattegrond</Link>
-          </li>
-          <li>
-            <Link href="/organisatie">organisatie</Link>
-          </li>
+          <div className="flex gap-4">
+            <li>
+              <Link href="/ruimtes">ruimtes</Link>
+            </li>
+            <li>
+              <Link href="/plattegrond">plattegrond</Link>
+            </li>
+            <li>
+              <Link href="/organisatie">organisatie</Link>
+            </li>
+          </div>
         </ul>
       </header>
       <main className="bg-gray-100 flex-1 p-2">{children}</main>
