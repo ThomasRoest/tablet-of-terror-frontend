b00aa86eb6b03f3fd34e57a38be79766f583c122
Thomas Roest <thomas.roest@moxio.com>
2024-11-12 14:33:31 +0100

    with selection

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,6 +1,8 @@
+import { FormEvent, useCallback, useState } from "react";
 import { Button } from "./ui/button";
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogHeader,
   DialogTitle,
@@ -14,79 +16,101 @@ import {
   SelectValue,
 } from "./ui/select";
 
-export const BookingForm = ({ value }: { value: string }) => {
+export const BookingForm = ({
+  value,
+  onSubmit,
+}: {
+  value: string;
+  onSubmit: (name: string, value: string) => void;
+}) => {
+  const [naam, setNaam] = useState("");
+
+  const handleSubmit = useCallback(
+    (event: FormEvent) => {
+      event.preventDefault();
+
+      onSubmit(naam, value);
+    },
+    [naam, onSubmit, value]
+  );
+
   return (
     <Dialog>
-      <DialogTrigger className="h-full w-full hover:bg-emerald-500 transition">
+      <DialogTrigger className="h-full w-full hover:bg-gray-300 transition">
         {value}
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Reserveren: {value}</DialogTitle>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="booker" className="text-right">
-              Naam
-            </label>
-            <Select>
-              <SelectTrigger className="col-span-3">
-                <SelectValue placeholder="Selecteer" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="Thomas">Thomas</SelectItem>
-                <SelectItem value="Annemiek">Annemiek</SelectItem>
-                <SelectItem value="Andre">Andre</SelectItem>
-                <SelectItem value="Tom">Tom</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
 
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="time" className="text-right">
-              Tijd
-            </label>
-            <Select>
-              <SelectTrigger id="time" className="col-span-3">
-                <SelectValue placeholder="Selecteer" />
-              </SelectTrigger>
-              <SelectContent>
-                {Array.from({ length: 24 }, (_, i) => i).map((hour) => (
-                  <SelectItem
-                    key={hour}
-                    value={`${hour.toString().padStart(2, "0")}:00`}
-                  >
-                    {`${hour.toString().padStart(2, "0")}:00`}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </Select>
+      <DialogContent className="sm:max-w-[425px]">
+        <form onSubmit={handleSubmit}>
+          <DialogHeader>
+            <DialogTitle>Reserveren: {value}</DialogTitle>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="booker" className="text-right">
+                Naam
+              </label>
+              <Select
+                onValueChange={(value) => {
+                  setNaam(value);
+                }}
+              >
+                <SelectTrigger className="col-span-3">
+                  <SelectValue placeholder="Selecteer" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="Thomas">Thomas</SelectItem>
+                  <SelectItem value="Annemiek">Annemiek</SelectItem>
+                  <SelectItem value="Andre">Andre</SelectItem>
+                  <SelectItem value="Tom">Tom</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="time" className="text-right">
+                Tijd
+              </label>
+              <Select>
+                <SelectTrigger id="time" className="col-span-3">
+                  <SelectValue placeholder="Selecteer" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Array.from({ length: 24 }, (_, i) => i).map((hour) => (
+                    <SelectItem
+                      key={hour}
+                      value={`${hour.toString().padStart(2, "0")}:00`}
+                    >
+                      {`${hour.toString().padStart(2, "0")}:00`}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <label htmlFor="duration" className="text-right">
+                Duur
+              </label>
+              <Select>
+                <SelectTrigger id="duration" className="col-span-3">
+                  <SelectValue placeholder="Select duration" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="30">30 minuten</SelectItem>
+                  <SelectItem value="60">1 uur</SelectItem>
+                  <SelectItem value="90">1.5 uur</SelectItem>
+                  <SelectItem value="120">2 uur</SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <label htmlFor="duration" className="text-right">
-              Duur
-            </label>
-            <Select>
-              <SelectTrigger id="duration" className="col-span-3">
-                <SelectValue placeholder="Select duration" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="30">30 minuten</SelectItem>
-                <SelectItem value="60">1 uur</SelectItem>
-                <SelectItem value="90">1.5 uur</SelectItem>
-                <SelectItem value="120">2 uur</SelectItem>
-              </SelectContent>
-            </Select>
+          <div className="flex justify-end">
+            <DialogClose asChild>
+              <Button type="submit">
+                Reserveer
+              </Button>
+            </DialogClose>
           </div>
-        </div>
-        <div className="flex justify-end">
-          <Button
-            type="button"
-            onClick={() => console.log("Booking submitted")}
-          >
-            Reserveer
-          </Button>
-        </div>
+        </form>
       </DialogContent>
     </Dialog>
   );
--- src/components/Magic.tsx
+++ src/components/Magic.tsx
@@ -1,22 +1,48 @@
+import { useState } from "react";
 import { BookingForm } from "./BookingForm";
 
 export const Magic = () => {
+  const [selectedTables, setSelectedTables] = useState<
+    { value: string; name: string }[]
+  >([]);
+
+  const isSelected = (value: string): boolean => {
+    return selectedTables.map((item) => item.value).includes(value);
+  };
+
+  const onSubmit = (name: string, value: string): void => {
+    setSelectedTables((state) => {
+      return [...state, { name, value }];
+    });
+  };
   return (
     <div className="col-span-4 row-span-7 font-semibold grid grid-cols-4 border gap-1">
       <div className="bg-white col-span-4  flex items-center justify-center" />
 
       <div className="bg-white col-span-1 row-span-4 flex items-center justify-center" />
-      <div className="col-span-1 row-span-2 flex items-center justify-center bg-emerald-300 rounded">
-        <BookingForm value="L" />
+      <div
+        className={`col-span-1 row-span-2 flex items-center justify-center  rounded ${
+          isSelected("L") ? "bg-red-300" : "bg-emerald-300"
+        }`}
+      >
+        <BookingForm value="L" onSubmit={onSubmit} />
       </div>
       <div className="bg-white col-span-1 row-span-2 flex items-center justify-center" />
 
       <div className="bg-white col-span-1 row-span-4  flex items-center justify-center" />
-      <div className="col-span-1 row-span-2 flex items-center justify-center bg-emerald-300 rounded">
-        <BookingForm value="M" />
+      <div
+        className={`col-span-1 row-span-2 flex items-center justify-center  rounded ${
+          isSelected("M") ? "bg-red-300" : "bg-emerald-300"
+        }`}
+      >
+        <BookingForm value="M" onSubmit={onSubmit} />
       </div>
-      <div className="col-span-1 row-span-2 flex items-center justify-center bg-emerald-300 rounded">
-        <BookingForm value="N" />
+      <div
+        className={`col-span-1 row-span-2 flex items-center justify-center  rounded ${
+          isSelected("N") ? "bg-red-300" : "bg-emerald-300"
+        }`}
+      >
+        <BookingForm value="N" onSubmit={onSubmit} />
       </div>
       <div className="bg-white col-span-4 flex items-center justify-center ">
         <span className="text-sm text-gray-500">Magic</span>
