02a18adf308d1e865225731af974dcd85be9237f
Peter van Dommelen <peter.vandommelen@moxio.com>
2024-11-12 14:56:31 +0100

    Some calls to static data.

---
+++ src/lib/server.ts
@@ -0,0 +1,40 @@
+
+export type Werknemer = {
+	naam: string;
+};
+
+export type Bureau = {
+	laptopdockOrientatie: "links"|"rechts"|"onbekend"|"geen"
+}
+
+export type Vergaderruimte = {
+	naam: string,
+	capaciteit: number
+}
+
+async function get(url): Promise<any> {
+	const response = await fetch(url);
+	return await response.json();
+}
+
+export function getWerknemers(): Promise<Record<string, Werknemer>> {
+	return get("http://141.138.139.147:5048/Werknemers")
+		.then((data) => {
+			return data;
+		})
+}
+
+export function getBureaus(): Promise<Record<string, Bureau>> {
+	return get("http://141.138.139.147:5048/Bureaus")
+		.then((data) => {
+			return data;
+		})
+}
+
+
+export function getVergaderruimtes(): Promise<Record<string, Vergaderruimte>> {
+	return get("http://141.138.139.147:5048/Vergaderruimtes")
+		.then((data) => {
+			return data;
+		})
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,29 @@
+import { Bureau, getBureaus, getVergaderruimtes, getWerknemers, Vergaderruimte, Werknemer } from "@/lib/server.ts";
+import { useEffect, useState } from "react";
 
 export const HomePage = () => {
+	const [ werknemers, setWerknemers ] = useState<Record<string, Werknemer>|undefined>();
+	useEffect(() => {
+		getWerknemers().then(setWerknemers);
+	}, []);
+	const [ bureaus, setBureaus ] = useState<Record<string, Bureau>|undefined>();
+	useEffect(() => {
+		getBureaus().then(setBureaus);
+	}, []);
+	const [ vergaderruimtes, setVergaderruimtes ] = useState<Record<string, Vergaderruimte>|undefined>();
+	useEffect(() => {
+		getVergaderruimtes().then(setVergaderruimtes);
+	}, []);
+
+	useEffect(() => {
+		console.log("bureaus", bureaus);
+	}, [werknemers]);
+	useEffect(() => {
+		console.log("werknemers", werknemers);
+	}, [werknemers]);
+	useEffect(() => {
+		console.log("vergaderruimtes", vergaderruimtes);
+	}, [werknemers]);
+
     return <div>home</div>;
 };
